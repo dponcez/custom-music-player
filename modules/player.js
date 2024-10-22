fdc7eaffb15c9294f6c83be41882a8f7e421aa3c
Damian Ponce <dponcez_git@hotmail.com>
2024-10-21 22:23:56 -0500

    refactor: change function (handlePlay) to (handlePlaySong)

--- modules/player.js
+++ modules/player.js
@@ -3,6 +3,7 @@ import { selector } from "./custom_functions.js";
 import { selectorAll } from "./custom_functions.js";
 import { eventHandler } from "./custom_functions.js";
 import { debounce } from "../utils/debounce.js";
+import { formatTime } from "../utils/formatTime.js";
 
 export const player = () => {
 
@@ -76,32 +77,33 @@ export const player = () => {
       const response = await fetch(requestURL);
       const json = await response.json();
       playlist = json
-      loadCurrentSong(playlist)
+      loadCurrentSong(playlist[index])
     } catch (error) {
       log(`Failure to load data: ${error}`)
     }
   }
 
   fetchData();
-
+  
   const loadCurrentSong = (current) => {
-    const {"playlist": [{artist, song, title, poster}]} = current;
+    const { artist, song, title, poster } = current;
+
     try {
-      cover.style.backgroundImage = `url(${poster})`
+      cover.style.backgroundImage = `url(${poster})`;
       artistName.innerText = `${artist}`;
       songName.innerText = `${title}`;
 
-      audio.src = `${song}`
-
-    } catch (error) {
-      log(error.message)
+      audio.src = `${song}`;
+    } catch(error) {
+      log(`Failure to parse the data: ${error}`)
     }
-    
   }
-
+  
   const playSong = () => {
     outerPlate.classList.add('play');
     tonearm.classList.add('active');
+    featureContainer.classList.add('visible');
+
     playBtn.innerHTML = pause;
 
     audio.play()
@@ -110,12 +112,13 @@ export const player = () => {
   const pauseSong = () => {
     outerPlate.classList.remove('play');
     tonearm.classList.remove('active');
+    featureContainer.classList.remove('visible');
 
     playBtn.innerHTML = play;
     audio.pause()
   };
 
-  const handlePlay = () => {
+  const handlePlaySong = () => {
     if(!isPlaying){
       isPlaying = true;
       playSong()
@@ -123,9 +126,48 @@ export const player = () => {
       isPlaying = false;
       pauseSong()
     }
-    // log('clicked')
   }
 
+  const handlePrevSong = () => {
+    index--;
+
+    if(index < 0) playlist.length - 1;
+    audio.currentTime = 0;
+
+    loadCurrentSong(playlist[index]);
+    playSong()
+  }
+
+  const handleNextSong = () => {
+    index++;
+
+    if(index > playlist.length - 1) index = 0;
+    audio.currentTime = 0;
+
+    loadCurrentSong(playlist[index]);
+    playSong()
+  }
+
+  const setTimeUpdate = (e) => {
+    const { currentTime, duration } = e.srcElement;
+    const percent = ( currentTime / duration ) * 100;
+    progress.style.width = `${percent}%`
+  }
+
+  const updateProgressBar = (e) => {
+    const width = e.offsetWidth;
+    const offset = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (offset / width) * duration;
+  }
+
+  let timeout = 300;
+
   // events
-  eventHandler(playBtn, 'click', debounce(() => handlePlay()), 300)
+  eventHandler(playBtn, 'click', debounce(() => handlePlaySong()), timeout);
+  eventHandler(forwardBtn, 'click', debounce(() => handleNextSong()), timeout);
+  eventHandler(backwardBtn, 'click', debounce(() => handlePrevSong()), timeout);
+  eventHandler(audio, 'timeupdate', setTimeUpdate);
+  eventHandler(progressBar, 'click', updateProgressBar)
 }
