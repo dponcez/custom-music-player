e3984826053642f79f36805a1eb7f3baa890cea9
Damian Ponce <dponcez_git@hotmail.com>
2024-10-26 19:37:03 -0500

    feat: add function (handleDarkMode) to script.js file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 import { template } from "../modules/template.js";
 import { player } from "../modules/player.js";
+import { handleDarkMode } from "../hooks/theme.js";
+
 template();
-player()
+player();
+handleDarkMode('dark', 'active', ['[data-dark-mode]', '.main', '.mode', '.inactive']);
